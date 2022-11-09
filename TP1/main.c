de608d66871e23c13448529699a084ad76e3d9d3
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-11-09 15:54:27 +0100

    Update main.c

--- TP1/main.c
+++ TP1/main.c
@@ -92,4 +92,17 @@ int ajouterSansDoublon(Client c, Tab t, int * nbClients) {
     ajouter(c, t, nbClients);
     return 1;
 }
+int enleverClient(int idClient, Tab t, int * nbClients) {
+    int i, j;
+    for (i = 0; i < *nbClients; i++) {
+        if (t[i].idClient == idClient) {
+            for (j = i; j < *nbClients - 1; j++) {
+                t[j] = t[j + 1];
+            }
+            (*nbClients)--;
+            return 1;
+        }
+    }
+    return 0;
+}
 
