5b4c6b1766709ea9fdd07a564d8fb15d918d2c77
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-11-09 15:50:00 +0100

    Update main.c

--- TP1/main.c
+++ TP1/main.c
@@ -69,4 +69,17 @@ void rechercheIdMinMax(Tab t, int nbClients, int * indiceIdMin, int * indiceIdMa
         }
     }
 }
+int afficherDoublons(Tab t, int nbClients) {
+    int i, j;
+    int nbDoublons = 0;
+    for (i = 0; i < nbClients; i++) {
+        for (j = i + 1; j < nbClients; j++) {
+            if (t[i].idClient == t[j].idClient && strcmp(t[i].nomClient, t[j].nomClient) == 0) {
+                printf("Doublon : %d %s \n", t[i].idClient, t[i].nomClient);
+                nbDoublons++;
+            }
+        }
+    }
+    return nbDoublons;
+}
 
