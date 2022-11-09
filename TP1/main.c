7bbe5f09d9bc010192b9aef4ad89f74689347c98
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-11-09 15:46:23 +0100

    Update main.c

--- TP1/main.c
+++ TP1/main.c
@@ -54,4 +54,19 @@ int rechercheClient(int idClient, Tab t, int nbClients, int * indiceClient) {
     }
     return 0;
 }
+void rechercheIdMinMax(Tab t, int nbClients, int * indiceIdMin, int * indiceIdMax) {
+    int i;
+    int idMin = t[0].idClient;
+    int idMax = t[0].idClient;
+    for (i = 0; i < nbClients; i++) {
+        if (t[i].idClient < idMin) {
+            idMin = t[i].idClient;
+            *indiceIdMin = i;
+        }
+        if (t[i].idClient > idMax) {
+            idMax = t[i].idClient;
+            *indiceIdMax = i;
+        }
+    }
+}
 
