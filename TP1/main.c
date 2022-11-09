3f9092561c5f2c3cf351da7a90fe302c7bdb7f0d
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-11-09 15:57:29 +0100

    Update main.c

--- TP1/main.c
+++ TP1/main.c
@@ -105,4 +105,44 @@ int enleverClient(int idClient, Tab t, int * nbClients) {
     }
     return 0;
 }
+int main() {
+    Tab t;
+    int nbClients = 0;
+    int indiceClient;
+    int indiceIdMin;
+    int indiceIdMax;
+    int nbDoublons;
+    int idClient;
+    Client c;
+    saisieClients(t, 3, &nbClients);
+    afficherClients(t, nbClients);
+    printf("Saisir l'identifiant du client à modifier : ");
+    scanf("%d", &idClient);
+    if (rechercheClient(idClient, t, nbClients, &indiceClient)) {
+        modifierNom(t, nbClients, indiceClient);
+    }
+    afficherClients(t, nbClients);
+    rechercheIdMinMax(t, nbClients, &indiceIdMin, &indiceIdMax);
+    printf("Client avec l'identifiant le plus petit : %d %s \n", t[indiceIdMin].idClient, t[indiceIdMin].nomClient);
+    printf("Client avec l'identifiant le plus grand : %d %s \n", t[indiceIdMax].idClient, t[indiceIdMax].nomClient);
+    nbDoublons = afficherDoublons(t, nbClients);
+    printf("Nombre de doublons : %d \n", nbDoublons);
+    saisie(&c);
+    if (ajouterSansDoublon(c, t, &nbClients)) {
+        printf("Client ajouté \n");
+    } else {
+        printf("Client non ajouté \n");
+    }
+    afficherClients(t, nbClients);
+    printf("Saisir l'identifiant du client à enlever : ");
+    scanf("%d", &idClient);
+    if (enleverClient(idClient, t, &nbClients)) {
+        printf("Client enlevé \n");
+    } else {
+        printf("Client non enlevé \n");
+    }
+    afficherClients(t, nbClients);
+    return 0;
+}
+
 
