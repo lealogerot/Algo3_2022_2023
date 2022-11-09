5bb3a3e22310ba67517b307d97f8af2ae64a1226
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-11-09 15:40:59 +0100

    Update main.c

--- TP1/main.c
+++ TP1/main.c
@@ -38,3 +38,10 @@ void modifierNom(Tab t, int nbClients, int i) {
         scanf("%s", t[i].nomClient);
     }
 }
+void afficherClients(Tab t, int nbClients) {
+    int i;
+    for (i = 0; i < nbClients; i++) {
+        printf("Client %d : %d %s \n", i, t[i].idClient, t[i].nomClient);
+    }
+}
+
