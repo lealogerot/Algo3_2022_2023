0269f3151f41e0619670073ec21945e560d97064
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-11-16 15:59:21 +0100

    Update main.c

--- TP2/main.c
+++ TP2/main.c
@@ -84,3 +84,16 @@ int rechercheDicho(Tab t, int n ,int i,int v)
     }
     return res;
 }
+int main()
+{
+    Tab t1 = {4,2,8,0,7,3,1,5,9,6};
+    Tab t2 = {6,12,8,52,41,2,7,23,16,25};
+    printf ("La somme des entiers du tableau t1 = %d\n",somme(t1,N,0)); // Avec 3 paramètres
+    afficher(&t1, N, 0);
+    printf("\nLa somme des entiers du tableau t1 est %d\n",sum2Parameters(t1,N)); // Avec 2 paramètres
+    printf("Le nombre d'elements du tableau superieur a 6 est %d\n",vSup(t1, N, 0, 6));
+    decroissant(t1,N,N-1);
+    printf("L'indice de la valeur 6 est %d\n",rechercheV(t1,N,0,6));
+    printf("L'indice de la valeur 12 est %d\n",rechercheDicho(t2,N,0,12));
+    return 0;
+}
