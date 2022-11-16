9f93cb2141a6ec2a761b7207ce0511f2eda9f458
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-11-16 15:00:29 +0100

    Update main.c

--- TP2/main.c
+++ TP2/main.c
@@ -3,13 +3,13 @@
 typedef int Tab[N];
 int somme(Tab t, int n, int i ){
     int res=0;
-printf ("Calcul de la somme a partir de l’indice %d :\n",i);
+printf ("Calcul de la somme a partir de lâ€™indice %d :\n",i);
     if (i<n){
-printf ("\t appel du calcul de la somme a partir de l’indice %d :\n",i+1);
+printf ("\t appel du calcul de la somme a partir de lâ€™indice %d :\n",i+1);
 res=t[i]+somme(t,n,i+1);
 printf ("\t addition avec t[%d]\n",i );
 }
-printf ("\t Somme a partir de l’indice %d = %d\n\n",i,res);
+printf ("\t Somme a partir de lâ€™indice %d = %d\n\n",i,res);
 return res ;
 }
 void afficher(int *t, int n, int i){
@@ -19,9 +19,4 @@ void afficher(int *t, int n, int i){
     printf("%d", t[0]);
     afficher(t+1, n, i+1);
 }
-int main(){
-Tab t1 = {4,2,8,0,7,3,1,5,9,6};
-printf ("La somme des entiers du tableau t1 = %d\n",somme(t1,N,0));
-afficher(&t1, N, 0);
-return 0;
-}
+
