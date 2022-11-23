ff8c0ceaf66a847f65b79f68143cc18dbb350b2f
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-11-23 15:24:30 +0100

    Update main.c

--- TP3/main.c
+++ TP3/main.c
@@ -42,19 +42,44 @@ void triSelection(Tab t, int n)
     }
 }
 
+void triRapide(Tab t, int n)
+{
+    int i, j, pivot, tmp;
+    if (n < 2)
+        return;
+    pivot = t[n / 2];
+    for (i = 0, j = n - 1;; i++, j--)
+    {
+        while (t[i] < pivot)
+            i++;
+        while (t[j] > pivot)
+            j--;
+        if (i >= j)
+            break;
+        tmp = t[i];
+        t[i] = t[j];
+        t[j] = tmp;
+    }
+    triRapide(t, i);
+    triRapide(t + i, n - i);
+}
+
 int main()
 {   Tab t1, t2;
     remplirTableau(t1, N);
+    printf ("tableau t1 : ");
     afficheTableau(t1, N);
     copieTableau(t1, t2, N);
+    printf ("tableau t2 copie de t1 : ");
     afficheTableau(t2, N);
      printf("Tableau t1 non trié :\n");
     afficheTableau(t1, N);
     printf("Tableau t1 trié par sélection :\n");
     triSelection(t1, N);
     afficheTableau(t1, N);
+    printf("Tableau t2 trié par tri rapide :\n");
+    triRapide(t2, N);
+    afficheTableau(t2, N);
     return 0;
 }
-
-
 
