247085de180bcf0dd9b3cf4dfa4384d31cf817ef
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-11-23 15:02:13 +0100

    Update main.c

--- TP3/main.c
+++ TP3/main.c
@@ -4,4 +4,30 @@
 
 typedef int Tab[N];
 
+void remplirTableau(Tab t, int n)
+{
+    int i;
+    srand(time(NULL));
+    for (i = 0; i < n; i++)
+        t[i] = rand() % n;
+}
+
+void afficheTableau(Tab t, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", t[i]);
+    printf("\n");
+}
+
+
+
+
+int main()
+{   Tab t1 ;
+    remplirTableau(t1, N);
+    afficheTableau(t1, N);
+    return 0;
+}
+
 
