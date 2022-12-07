6a9ab6f772e20e9984091cea7959f48bf4602f08
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-12-07 14:57:00 +0100

    Update main.c

--- TP4/main.c
+++ TP4/main.c
@@ -16,6 +16,19 @@ void ajouteClientFichier(Client c, char *nomFichier) {
     fprintf(f, "%d %s ", c.id, c.nom);
     fclose(f);
 }
+void afficheClientsFichier(char *nomFichier) {
+    FILE *f = fopen(nomFichier, "r");
+    if (f == NULL) {
+        printf("Erreur de l'ouverture du fichier %s", nomFichier);
+        exit(1);
+    }
+    Client c;
+    while (fscanf(f, "%d %s ", &c.id, c.nom) != EOF) {
+        printf("%d %s ", c.id, c.nom);
+    }
+
+    fclose(f);
+}
 
 int main(){
 return 0 ;
