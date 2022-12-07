2f53244695ccce3ddbab9f64d2e8b3760dc02d69
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-12-07 14:54:49 +0100

    Update main.c

--- TP4/main.c
+++ TP4/main.c
@@ -7,4 +7,17 @@ typedef struct {
                 char nom[chaine];
                 } Client;
 
+void ajouteClientFichier(Client c, char *nomFichier) {
+    FILE *f = fopen(nomFichier, "a");
+    if (f == NULL) {
+        printf("Erreur de l'ouverture du fichier %s", nomFichier);
+        exit(1);
+    }
+    fprintf(f, "%d %s ", c.id, c.nom);
+    fclose(f);
+}
+
+int main(){
+return 0 ;
+}
 
