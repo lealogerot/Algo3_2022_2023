084a7785b2c3178d8c1c2f7e81d3cf782c985519
lealogerot <113138463+lealogerot@users.noreply.github.com>
2022-12-07 15:00:55 +0100

    Update main.c

--- TP4/main.c
+++ TP4/main.c
@@ -43,6 +43,25 @@ int nombreClientsFichier(char *nomFichier) {
     fclose(f);
     return nb;
 }
+void appartientClientFichier(char *nomClient, char *nomFichier) {
+    FILE *f = fopen(nomFichier, "r");
+    if (f == NULL) {
+        printf("Erreur d'ouverture du fichier %s", nomFichier);
+        exit(1);
+    }
+    Client c;
+    int trouve = 0;
+    while (fscanf(f, "%d %s ", &c.id, c.nom) != EOF) {
+        if (strcmp(c.nom, nomClient) == 0) {
+            printf("%d %s ", c.id, c.nom);
+            trouve = 1;
+        }
+    }
+    if (trouve == 0) {
+        printf("Client non répertorié");
+    }
+    fclose(f);
+}
 int main(){
 return 0 ;
 }
